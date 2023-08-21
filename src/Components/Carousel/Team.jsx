3c6692347dee423a2c97c885df3b280a2d78f8aa
K Balaji Naik <101827986+Balaji91221@users.noreply.github.com>
2023-08-21 22:03:54 +0530

    Team info+

--- src/Components/Carousel/Team.jsx
+++ src/Components/Carousel/Team.jsx
@@ -1,16 +1,92 @@
-import React from 'react';
-import './team.css';
+import React from "react";
+import "./team.css";
 
 const Team = () => {
+  const committeeMembers = [
+    {
+      role: "Chairman",
+      name: "Dr. G. Viswanathan,",
+      position: "Chancellor VIT Group",
+    },
+    {
+      role: "Vice Chairman",
+      name: "Sankar Viswanathan, Sekar Viswanathan, Selvam Viswanathan",
+      position: "VP, VIT",
+    },
+    {
+      role: "Treasurer",
+      name: "Dr S V Kota Reddy",
+      position: "Vice Chancellor ",
+    },
+    { role: "Member", name: "Director 1", position: "VTBIF" },
+    { role: "Member", name: " Director 2", position: "VTBIF, Director 1" },
+    { role: "Member", name: " Registrar", position: "VIT-AP University" },
+    {
+      role: "Member Secretary",
+      name: " Director 2",
+      position: "Director IIEC",
+    },
+    {
+      role: "Member-Central Govt. Institute",
+      name: " Dr. Mukesh Tripati",
+      position: "Director & CEO AIIMS Mangalagiri, AP",
+    },
+    {
+      role: "Member Govt of AP",
+      name: " Mr. Anil Kumar Tentu CEO",
+      position: "AP Innovation Society (APIS), Govt. of Andhra Pradesh",
+    },
+    {
+      role: "Member-Angel investor",
+      name: " Mr. Bhanu Prakash- Angel Investor",
+      position: "Partner & Director-Plural Technology Pvt. Ltd.",
+    },
+    {
+      role: "Member-VIT Group",
+      name: " Dr. Balachandran ",
+      position: "A-VIT TBI incubation Centre",
+    },
+    {
+      role: "Member-Industry Expert",
+      name: " Mr. Sridhar Kosaraju",
+      position: " Founder & CEO@Nimaisoft | Chairman DeepTech & Naipunya Former State President@ITAAP | Al Evangelist | Angel Investor | Mentor",
+    },
+    {
+      role: "Member-Ecosystem",
+      name: " Mr. Ravi Eswarapu - TIEAP President)",
+      position: "CEO HUB (Athra University)",
+    },
+    {
+      role: "Member",
+      name: "  ",
+      position: "Deputy Director IEC",
+    },
+    {
+      role: "Member",
+      name: " ",
+      position: "CFO",
+    },
+  ];
   return (
     <div id="team" className="team">
+  <div className="advisory-committee">
+            <h2>VTBIF Proposed Advisory Committee</h2>
+            <ul>
+                {committeeMembers.map((member, index) => (
+                    <li key={index}>
+                        <strong>{member.role}:</strong> {member.name}{" "}
+                        {member.position && <span className="position">({member.position})</span>}
+                    </li>
+                ))}
+            </ul>
+        </div>
       <h4 className="headings">TEAM</h4>
-      <div className='m'>
+      <div className="m">
         <div className="row">
           {/* Card 1 */}
           <div className="teamcard">
             <div className="image">
-            <img src="Images/ameet.jpeg" alt="" />
+              <img src="Images/ameet.jpeg" alt="" />
             </div>
             <div className="info">
               <h3 className="u">Dr. Ameet Chavan</h3>
@@ -24,7 +100,9 @@ const Team = () => {
             </div>
             <div className="info">
               <h3 className="u">Dr. Sudha Ellison Mathe</h3>
-              <span className="r">VLSI Architectures, FPGA Design, Digital IC Design</span>
+              <span className="r">
+                VLSI Architectures, FPGA Design, Digital IC Design
+              </span>
             </div>
           </div>
           <div className="teamcard">
@@ -32,8 +110,7 @@ const Team = () => {
               <img src="Images/ravi.jpg" alt="" />
             </div>
             <div className="info">
-              <h3 className="u">Dr. M Ravindra
-</h3>
+              <h3 className="u">Dr. M Ravindra</h3>
               <span className="r"></span>
             </div>
           </div>
@@ -54,10 +131,8 @@ const Team = () => {
             </div>
             <div className="info">
               <h3 className="u">S. Sibi Chakkaravarthy</h3>
-              
             </div>
           </div>
-
 
           <div className="teamcard">
             <div className="image">
@@ -65,16 +140,14 @@ const Team = () => {
             </div>
             <div className="info">
               <h3 className="u">Prof. Samuel Johnson</h3>
-             
             </div>
-            </div>
-            <div className="teamcard">
+          </div>
+          <div className="teamcard">
             <div className="image">
               <img src="Images/sira.jpg" alt="" />
             </div>
             <div className="info">
               <h3 className="u">Dr. Y Md. Sirajudeen</h3>
-              
             </div>
           </div>
 
@@ -92,8 +165,7 @@ const Team = () => {
               <img src="Images/ani.jpg" alt="" />
             </div>
             <div className="info">
-              <h3 className="u">Dr. Anindita Shome
-</h3>
+              <h3 className="u">Dr. Anindita Shome</h3>
             </div>
           </div>
 
@@ -102,8 +174,7 @@ const Team = () => {
               <img src="Images/ana.jpg" alt="" />
             </div>
             <div className="info">
-              <h3 className="u">Prof. Ananthu S. Hari
-</h3>
+              <h3 className="u">Prof. Ananthu S. Hari</h3>
             </div>
           </div>
 
@@ -160,12 +231,9 @@ const Team = () => {
               <h3 className="u">Dr. Renuprasad Patki</h3>
             </div>
           </div>
-
-
-          </div>
         </div>
       </div>
-   
+    </div>
   );
 };
 
